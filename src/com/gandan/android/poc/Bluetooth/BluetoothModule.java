12601ed88bf98d8c6e0054f1aeacc566770dddad
Chandra <otama.ariq@gmail.com>
2013-10-13 20:50:25 +0700

    rename Bluetooth directory into bluetooth

--- src/com/gandan/android/poc/Bluetooth/BluetoothModule.java
+++ src/com/gandan/android/poc/Bluetooth/BluetoothModule.java
@@ -9,16 +9,25 @@ import android.bluetooth.BluetoothAdapter;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.content.Context;
+import android.content.BroadcastReceiver;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.lang.NullPointerException;
 
-public class BluetoothModule {
+public class BluetoothModule extends BroadcastReceiver {
+    private static final String  TAG = BluetoothModule.class.getSimpleName();
     private final BluetoothAdapter mBluetoothAdapter;
     private final static int REQUEST_ENABLE_BT = 1;
-    
+    private boolean mEnabled = false;
+    private Context mContext;
+
     public BluetoothModule(Activity activity) {
+	mContext = (Context) activity;
+
 	// get BluetoothAdapter
 	if(VERSION.SDK_INT< VERSION_CODES.JELLY_BEAN_MR1) {
 	    // sdk <= 17 use getDefaultAdapter
@@ -36,10 +45,32 @@ public class BluetoothModule {
 	if(!mBluetoothAdapter.isEnabled()) {
 	    Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 	    activity.startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+	} else {
+	    // change true when bluetooth state on
+	    mEnabled = true;
 	}
 	
     }
 
-    public void onActivityResult(int a, int b, Intent intent){
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+	if(requestCode == REQUEST_ENABLE_BT) {
+	    if(resultCode == Activity.RESULT_OK ) {
+		mEnabled = true;
+	    } else {
+		mEnabled = false;
+	    }
+	    Toast.makeText(mContext, "status " + mEnabled, Toast.LENGTH_LONG);
+	}
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+	if(intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF) == 
+	   BluetoothAdapter.STATE_ON){
+	    mEnabled = true;
+	} else {
+	    mEnabled = false;
+	}
+	Toast.makeText(mContext, "status " + mEnabled, Toast.LENGTH_LONG);
     }
 }
